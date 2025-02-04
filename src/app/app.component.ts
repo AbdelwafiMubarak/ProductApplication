a58e4c73e3b4770491d81ba5f6b8baa7a6332735
AbdelwafiMubarak <abdelwafi.mubarak@quadrabay.com>
2025-02-04 15:30:09 +0400

    stable2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./Component/navbar/navbar.component";
 import { RouterModule } from '@angular/router';
-import { ProductListComponent } from "./Component/productlist/productlist.component";
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [NavbarComponent, RouterModule, ProductListComponent]
+  imports: [NavbarComponent, RouterModule,]
 })
 export class AppComponent {
   title = 'ProductApplication';
